994b05fd82cdc60fbcfc9bf20f31f60d976255c3
Fikayo Adepoju <fikayo@Fikayos-MacBook-Pro.local>
2025-02-27 10:40:01 +0100

    Initial Commit

---
+++ models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    content: String,
+    timestamp: {type: Date, default: Date.now}
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post;
