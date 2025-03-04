138c4138d1c8b7ff7cacb2407dc725c011a29518
Fikayo Adepoju <fikayo@Fikayos-MacBook-Pro.local>
2025-03-04 09:47:48 +0100

    First Draft of Infinite Scrolling

--- server.js
+++ server.js
@@ -40,6 +40,53 @@ app.get("/seed", async (req, res) => {
     }
 })
 
+app.get("/posts", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 12;
+    const skip = (page - 1) * limit;
+
+    const posts = await Post.find()
+    .sort({timestamp: -1})
+    .skip(skip)
+    .limit(limit)
+    .lean()
+
+    return res.send(renderResults(posts, page, limit))
+})
+
+
+function renderResults (posts, page, limit) {
+
+    let html = '';
+
+    posts.forEach(post => {
+        html += `
+            <figure class="post-card">
+                <blockquote class="blockquote">
+                    <p>${post.title}</p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                    ${new Date(post.timestamp).toLocaleDateString()}
+                </figcaption>
+            </figure>
+        `
+    })
+
+    // Add trigger for the next page
+    if(posts.length === limit){
+        html += `
+            <div hx-get="/posts?page=${page + 1}" 
+                 hx-trigger="revealed delay:1000ms" 
+                 hx-swap="afterend" 
+                 class="htmx-indicator">
+                Loading more posts...
+            </div>
+        `;
+    }
+
+    return html;
+}
+
 //Run the server
 app.listen(PORT, () => {
     console.log(`Server running at port:${PORT}`)
